563926b3151e9f2c8fd90e84dede80719226f822
Michael Wallett <michael.wallett@gmail.com>
2012-06-30 22:03:32 +0100

    Examples of checked/unchecked

---
+++ TypeFundamentals/CheckedUncheckedTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using NUnit.Framework;
+using Shouldly;
+
+namespace TypeFundamentals
+{
+    [TestFixture]
+    public class CheckedUncheckedTests
+    {
+        [Test]
+        public void OverflowThrowsAnExceptionWithChecked()
+        {
+            Should.Throw<OverflowException>(ByteOverflowWithCheckedOperator);
+            Should.Throw<OverflowException>(ByteOverflowWithCheckedStatement);
+        }
+
+        private void ByteOverflowWithCheckedOperator()
+        {
+            byte i = byte.MaxValue;
+            i = checked((byte)(i + 1));
+        }
+
+        private void ByteOverflowWithCheckedStatement()
+        {
+            byte i = byte.MaxValue;
+
+            checked
+            {
+                i += 1;
+            }
+        }
+
+        [Test]
+        public void OverflowDoesNotThrowAnExceptionWithUnchecked()
+        {
+            // checked operator
+            byte i = byte.MaxValue;
+
+            i = unchecked((byte)(i + 1));
+
+            i.ShouldBe(byte.MinValue);
+
+            // checked statement
+            i = byte.MaxValue;
+
+            unchecked
+            {
+                i += 1;
+            }
+
+            i.ShouldBe(byte.MinValue);
+        }
+    }
+}
