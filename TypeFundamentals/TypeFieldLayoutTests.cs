c43d423465c366a161b90efbe5e5e737d41b6760
Michael Wallett <michael.wallett@gmail.com>
2012-07-01 17:38:54 +0100

    Added type field layout example

---
+++ TypeFundamentals/TypeFieldLayoutTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using NUnit.Framework;
+using Shouldly;
+
+namespace TypeFundamentals
+{
+    [TestFixture]
+    public class TypeFieldLayoutTests
+    {
+        [Test]
+        public void TypeLayoutCanBeControlledUsingStructLayoutAttribute()
+        {
+            ValueType value = new ValueType(1, 2);
+
+            value.X.ShouldBe(1);
+            value.Y.ShouldBe(2);
+        }
+
+        //[StructLayout(LayoutKind.Auto)] If the value type is not being used for unmanaged interop.
+        [StructLayout(LayoutKind.Explicit)]
+        public struct ValueType
+        {
+            [FieldOffset(0)]
+            private readonly int _x;
+
+            [FieldOffset(4)]
+            private readonly int _y;
+
+            public ValueType(int x, int y)
+            {
+                _x = x;
+                _y = y;
+            }
+
+            public int X
+            {
+                get { return _x; }
+            }
+
+            public int Y
+            {
+                get { return _y; }
+            }
+        }
+    }
+}
