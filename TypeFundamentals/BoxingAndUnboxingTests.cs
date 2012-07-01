31ee8aecd0273e45bad2723a529aaeadef8a8aa4
Michael Wallett <michael.wallett@gmail.com>
2012-07-01 18:30:58 +0100

    Started to add boxing/unboxing examples

---
+++ TypeFundamentals/BoxingAndUnboxingTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Shouldly;
+
+namespace TypeFundamentals
+{
+    [TestFixture]
+    public class BoxingAndUnboxingTests
+    {
+        [Test]
+        public void AValueTypeCannotBeUnboxedToADifferentType()
+        {
+            int x = 1;
+
+            // Box x
+            // 1) Allocate required memory on heap.
+            // 2) Copy fields to allocated memory.
+            // 3) Return a pointer to the object.
+            object o = x;
+                          
+            short y;
+
+            // Unbox o
+            Should.Throw<InvalidCastException>(() => y = (short)o);
+        }
+    }
+}
