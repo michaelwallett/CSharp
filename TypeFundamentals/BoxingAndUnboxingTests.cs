60aed4efa2d2083436572fbc557885c42790f850
Michael Wallett <michael.wallett@gmail.com>
2012-07-05 21:56:58 +0100

    Added another boxing/unboxing example

--- TypeFundamentals/BoxingAndUnboxingTests.cs
+++ TypeFundamentals/BoxingAndUnboxingTests.cs
@@ -26,5 +26,57 @@ namespace TypeFundamentals
             // Unbox o
             Should.Throw<InvalidCastException>(() => y = (short)o);
         }
+
+        [Test]
+        public void BoxingAndUnboxingExample()
+        {
+            Point p = new Point(1, 1);
+            p.ToString().ShouldBe("1, 1");
+
+            p.Change(2, 2);
+            p.ToString().ShouldBe("2, 2");
+
+            object o = p;
+            o.ToString().ShouldBe("2, 2");
+
+            ((Point)o).Change(3, 3);
+            o.ToString().ShouldBe("2, 2");
+
+            // Box p. Change the boxed object and discard it.
+            ((IChangeBoxedPoint)p).Change(4, 4);
+            p.ToString().ShouldBe("2, 2");
+
+            // Change the already boxed object on the heap.
+            ((IChangeBoxedPoint)o).Change(5, 5);
+            o.ToString().ShouldBe("5, 5");
+        }
+
+        public interface IChangeBoxedPoint
+        {
+            void Change(int x, int y);
+        }
+
+        public struct Point : IChangeBoxedPoint
+        {
+            private int _x;
+            private int _y;
+
+            public Point(int x, int y)
+            {
+                _x = x;
+                _y = y;
+            }
+
+            public void Change(int x, int y)
+            {
+                _x = x;
+                _y = y;
+            }
+
+            public override string ToString()
+            {
+                return string.Format("{0}, {1}", _x, _y);
+            }
+        }
     }
 }
