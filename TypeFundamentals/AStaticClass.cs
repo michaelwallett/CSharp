f9ea56c84945bbbda2f7927a42ca225da4d25abe
Michael Wallett <michael.wallett@gmail.com>
2012-07-17 21:42:13 +0100

    Added static class example

---
+++ TypeFundamentals/AStaticClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TypeFundamentals
+{
+    public static class AStaticClass
+    {
+        private static string aStaticField;
+
+        public static void AStaticMethod() { }
+
+        public static string AStaticProperty
+        {
+            get { return aStaticField; }
+            set { aStaticField = value; }
+        }
+
+        public static event EventHandler AStaticEvent;
+    }
+}
