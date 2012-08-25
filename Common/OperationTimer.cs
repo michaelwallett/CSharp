1c1b83f42466e7e1f782943a9760540df27e96b8
Michael Wallett <michael.wallett@gmail.com>
2012-08-25 16:07:45 +0100

    Added generic performace tests

---
+++ Common/OperationTimer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Common
+{
+    public class OperationTimer : IDisposable
+    {
+        private readonly string _text;
+        private readonly int _collectionCount;
+        private readonly Stopwatch _stopwatch;
+
+        public OperationTimer(string text)
+        {
+            PrepareForOperation();
+
+            _text = text;
+            _collectionCount = GC.CollectionCount(0);
+            _stopwatch = Stopwatch.StartNew();
+        }
+
+        public void Dispose()
+        {
+            _stopwatch.Stop();
+
+            Console.WriteLine("{0} milliseconds (GCs = {1}) {2}", _stopwatch.ElapsedMilliseconds, GC.CollectionCount(0) - _collectionCount, _text);
+        }
+
+        private void PrepareForOperation()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+        }
+    }
+}
---
+++ Generics/GenericPerformaceTests.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Common;
+using NUnit.Framework;
+
+namespace Generics
+{
+    [TestFixture]
+    public class GenericPerformaceTests
+    {
+        private const int Count = 1000000;
+
+        [Test]
+        public void TestPerformance()
+        {
+            TestValueTypePerformace();
+            TestReferenceTypePerformance();
+        }
+
+        private void TestValueTypePerformace()
+        {
+            using (new OperationTimer("List<int>"))
+            {
+                var l = new List<int>(Count);
+
+                for (int i = 0; i < Count; i++)
+                {
+                    l.Add(i);
+                    int x = l[i];
+                }
+
+                l = null;
+            }
+
+            using (new OperationTimer("ArrayList of In32"))
+            {
+                var a = new ArrayList(Count);
+
+                for (int i = 0; i < Count; i++)
+                {
+                    a.Add(i);
+                    int x = (int)a[i];
+                }
+
+                a = null;
+            }
+        }
+
+        private void TestReferenceTypePerformance()
+        {
+            using (new OperationTimer("List<string>"))
+            {
+                var l = new List<string>(Count);
+
+                for (int i = 0; i < Count; i++)
+                {
+                    l.Add("X");
+                    string x = l[i];
+                }
+
+                l = null;
+            }
+
+            using (new OperationTimer("ArrayList of String"))
+            {
+                var a = new ArrayList(Count);
+
+                for (int i = 0; i < Count; i++)
+                {
+                    a.Add("X");
+                    string x = (string)a[i];
+                }
+
+                a = null;
+            }
+        }
+    }
+}
