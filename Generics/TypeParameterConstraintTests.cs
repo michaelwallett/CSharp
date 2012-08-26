cde916cb8db8107eb428e4f92471e6d5c8913c57
Michael Wallett <michael.wallett@gmail.com>
2012-08-26 20:00:39 +0100

    Added example of type parameter constraints (naked constraint)

---
+++ Generics/TypeParameterConstraintTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit;
+using NUnit.Framework;
+using Shouldly;
+
+namespace Generics
+{
+    [TestFixture]
+    public class TypeParameterConstraintTests
+    {
+        [Test]
+        public void TestTypeParameterConstraints()
+        {
+            IList<string> list = new List<string>();
+            list.Add("X");
+
+            Convert<string, object>(list).First().ShouldBe("X");
+            Convert<string, IComparable>(list).First().ShouldBe("X");
+            Convert<string, IComparable<string>>(list).First().ShouldBe("X");
+            Convert<string, string>(list).First().ShouldBe("X");
+
+            // Error - No implicit conversion from string to Exception.
+            //Convert<string, Exception>(list).First().ShouldBe("X");
+        }
+
+        private static List<TResult> Convert<T, TResult>(IList<T> list) where T : TResult
+        {
+            var results = new List<TResult>(list.Count);
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                results.Add(list[i]);
+            }
+
+            return results;
+        }
+    }
+}
